c85e797e0650f1da69a57901ec7733ca02849f17
cbabi13 <babinchristopher34@gmail.com>
2017-04-24 11:15:50 -0500

    Updated WebForm3

---
+++ Project_IE2060_Christopher_Babin/WebForm3.aspx.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data.OleDb
+Public Class WebForm3
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        String connString = "Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=C:\\Users\\KevinDW\\Desktop\\dotNET\\Week 5\\Prak1\\demo1.accdb";
+
+        OleDbConnection conn = New OleDbConnection(connString);
+
+        conn.Open();
+
+        String sqlCmd = "SELECT CursusNaam FROM tblCursus";
+
+        OleDbCommand cmd = New OleDbCommand(sqlCmd, conn);
+
+        Using (OleDBDataReader reader = cmd.ExecuteReader())
+        {
+            ListBox1.Items.Add(reader);
+        }
+
+        conn.Close();
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
